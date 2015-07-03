472420b5c50aee841c7523f49d6f2875cb54e7a2
alexberk-krauss <ab3475@trevor.org>
2015-07-03 13:17:24 +0000

    more merge conflicts

--- Find_My_Park/application_controller.rb
+++ Find_My_Park/application_controller.rb
@@ -1,16 +1,10 @@
 require 'bundler'
 Bundler.require
 
-<<<<<<< HEAD
 require './models/neighberhood.rb'
 
 class MyApp < Sinatra::Base
 
-=======
-require './models/neighborhood.rb'
-
-class MyApp < Sinatra::Base
->>>>>>> 5be8d2f5c4ac7bf1a12f1c6596807059c9343fac
 
   get '/' do
     erb :index 
@@ -19,15 +13,7 @@ class MyApp < Sinatra::Base
   post '/results' do 
     @play_location = params["play_location"]
     @game_type = params["game_type"]
-<<<<<<< HEAD
-    @neighborhood = Neighborhood.new(params["play_location"])
-    @neighborhood.search
-    @neighborhood.get_venue_ids
-    @locations =  @neighborhood.venues
-    erb :results
-  end
-  
-=======
+
     @neighborhood = Neighborhood.new(params["play_location"], @game_type)
     @neighborhood.search
     @neighborhood.get_venue_ids
@@ -35,5 +21,5 @@ class MyApp < Sinatra::Base
     erb :results
   end
 
->>>>>>> 5be8d2f5c4ac7bf1a12f1c6596807059c9343fac
+
 end
